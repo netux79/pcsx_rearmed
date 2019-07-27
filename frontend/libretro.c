864a5f2cde78993f61fbf8bcf935c4c7a336342d
Joe Rmz <netux79@yahoo.com>
2019-07-27 12:11:19 -0500

    Small fix to hide option if dynachange recompiler is not enabled.

--- frontend/libretro.c
+++ frontend/libretro.c
@@ -462,9 +462,11 @@ void retro_set_environment(retro_environment_t cb)
       { "pcsx_rearmed_show_bios_bootlogo", "Show Bios Bootlogo(Breaks some games); disabled|enabled" },
       { "pcsx_rearmed_spu_reverb", "Sound: Reverb; enabled|disabled" },
       { "pcsx_rearmed_spu_interpolation", "Sound: Interpolation; simple|gaussian|cubic|off" },
+#ifndef DRC_DISABLE
       { "pcsx_rearmed_psxclock", "PSX CPU Clock (default 57); 57|58|59|60|61|62|63|64|65|66|67|68|69|70|71|72|73|74|75|76|77|78|79|80|81|82|83|84|85|86|87|88|89|90|91|92|93|94|95|96|97|98|99|100|30|31|32|33|34|35|36|37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56" },
       { "pcsx_rearmed_pe2_fix", "Parasite Eve 2 Fix (set PSX CPU Clock=70); disabled|enabled" },
       { "pcsx_rearmed_inuyasha_fix", "InuYasha Sengoku Battle Fix; disabled|enabled" },
+#endif
       { NULL, NULL },
    };
 
